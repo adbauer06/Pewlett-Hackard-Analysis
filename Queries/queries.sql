22e65baa39328d68b7c9fe379483e155390a0a04
Amy Bauer <adbauer06@gmail.com>
2021-08-13 23:31:31 -0500

    Upload deliverable 1

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -40,3 +40,102 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
   AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
   
 SELECT * from retirement_info;
+
+-- Joining departments and dept_manager tables
+SELECT d.dept_name,
+       dm.emp_no,
+       dm.from_date,
+       dm.to_date
+FROM departments d
+INNER JOIN dept_manager dm
+ON d.dept_no = dm.dept_no;
+
+-- Joining retirement_info and employee_dept tables
+SELECT ri.emp_no,
+       ri.first_name,
+       ri.last_name,
+       ed.to_date
+FROM retirement_info ri
+LEFT JOIN employee_dept ed
+ON ri.emp_no = ed.emp_no;
+
+
+-- Select current employee at retirement age
+SELECT ri.emp_no,
+       ri.first_name,
+	   ri.last_name,
+	   ed.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN employee_dept as ed
+ON ri.emp_no = ed.emp_no
+WHERE ed.to_date = ('9999-01-01');
+
+-- Employee count by department number
+SELECT COUNT(ce.emp_no), ed.dept_no
+INTO retirees_dept
+FROM current_emp as ce
+LEFT JOIN employee_dept as ed
+ON ce.emp_no = ed.emp_no
+GROUP BY ed.dept_no
+ORDER BYed.dept_no;
+
+
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+SELECT e.emp_no, 
+       e.first_name, 
+	   e.last_name, 
+	   e.gender,
+	   s.salary,
+	   ed.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN employee_dept as ed
+ON (e.emp_no = ed.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (ed.to_date = '9999-01-01');
+
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+		
+
+SELECT ce.emp_no,
+       ce.first_name,
+       ce.last_name,
+       d.dept_name
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN employee_dept AS ed
+ON (ce.emp_no = ed.emp_no)
+INNER JOIN departments AS d
+ON (ed.dept_no = d.dept_no);
+
+
+-- Retirement info for only sales department
+SELECT ri.emp_no,
+       ri.first_name,
+	   ri.last_name,
+	   d.dept_name
+FROM retirement_info as ri
+INNER JOIN employee_dept AS ed
+ON(ri.emp_no = ed.emp_no)
+INNER JOIN departments as d
+ON (d.dept_no = ed.dept_no)
+WHERE d.dept_name IN ('Sales');
