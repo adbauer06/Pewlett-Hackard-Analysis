1b6abf5b8b2dfb8b76584786026fc80f63f8af55
Amy Bauer <adbauer06@gmail.com>
2021-08-13 23:48:30 -0500

    Upload deliverable 2 analysis

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -30,4 +30,23 @@ FROM unique_titles
 GROUP BY title
 ORDER BY 1 desc
 
+
+-- Create mentorship eligibility table
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+       e.first_name,
+	   e.last_name,
+	   e.birth_date,
+	   ed.from_date,
+	   ed.to_date,
+	   t.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN employee_dept as ed
+ON (e.emp_no = ed.emp_no)
+INNER JOIN titles as t
+ON (ed.emp_no = t.emp_no)
+WHERE (ed.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY 1; 
+
        
